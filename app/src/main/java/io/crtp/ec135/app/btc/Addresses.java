3949603519fb0572e0f681a4928ee697b5a9683c
ivwall <ivwall@tgndomains.com>
2022-09-20 13:51:06 -0400

    printing addresses

--- app/src/main/java/io/crtp/ec135/app/btc/Addresses.java
+++ app/src/main/java/io/crtp/ec135/app/btc/Addresses.java
@@ -78,7 +78,6 @@ public class Addresses {
         Date date = new Date(mediantime*1000L);
         String dateStr = ", "+sdf.format(date);
 
-        /*****
         JSONArray trxs = (JSONArray)((JSONObject)block.get("result")).get("tx");
         int cnt = trxs.size();
 
@@ -110,7 +109,7 @@ public class Addresses {
                         String addr = (String)addrs.get(k);
 
                         addrStr = addrString(addr);
-                        mySqlIDS.putAddrData(addrStr,"null");
+                        //mySqlIDS.putAddrData(addrStr,"null");
                         r2.append(addrStr);
 
                     }
@@ -137,6 +136,155 @@ public class Addresses {
             r2 = new StringBuffer();
             r2.append("         ");
         }
-        */
     }
+
+    public String blockNumString(int blk) {
+        String result = null;
+        try {
+            String blkString = ""+blk;
+            //                  12345678
+            String tempBuild = "        ";
+            int blkLength = blkString.length();
+            int diff = tempBuild.length() - blkLength;
+            StringBuffer buildStr = new StringBuffer();
+            for (int x=0; x<diff; x++){
+                buildStr.append(' ');
+            }
+            buildStr.append(blkString);
+            buildStr.append(',');
+            result = buildStr.toString();
+        } catch(Exception ex){
+            System.out.println(ex.toString());
+        }
+        return result;
+    }
+
+    public String trxNumString(int trxN){
+        String result = null;
+        try {
+            String trxStringIn = ""+trxN;
+            //                 12345
+            String buildStr = "     ";
+            int trxStringInLength = trxStringIn.length();
+            int diff = buildStr.length() - trxStringInLength;
+            StringBuffer stringBfr = new StringBuffer();
+            for (int x=0; x<diff; x++){
+                stringBfr.append(' ');
+            }
+            stringBfr.append(trxStringIn);
+            stringBfr.append(',');
+            result = stringBfr.toString();
+        } catch (Exception ex) {
+            System.out.println(ex.toString());
+        }
+        return result;
+    }
+
+    public String addrString(String addr){
+        StringBuffer result = new StringBuffer();
+        //              16   1         2         3         4         5         6       8  
+        //123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789
+        //         1         2         3         4         5         6         7         8         8         0
+        //                                                                              72
+        //  749151, 2342, bc1qwedezsjqyx5cl56dgzajtpawwste50gl05ty0t94zw38mp9zs64s7a5k8k, 1.473742370000
+        try {
+            //56789 1234567890123456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789
+            //     1         2         3         4         5         6         7         8         8         0
+            String te = "                                                               ";
+            int addrLength = addr.length();
+            int diff = te.length() - addrLength;
+            for (int x=0; x< diff; x++){
+                result.append(' ');
+            }
+            result.append(addr);
+            result.append(',');
+        } catch (Exception ex) {
+            System.out.println(ex.toString());
+        }
+        return result.toString();
+    }
+
+
+    public String asmWork(String asm){
+        String result = "not processed";
+        try {
+
+            String[] splited = asm.split(" ");
+
+            if ( splited[1].contains("OP_CHECKSIG") ) {
+                result = addressFromPubKey(splited[0]);
+            } else {
+                result = "asm: "+asm;
+            }
+
+        } catch ( Exception ex ){
+            result = asm;
+        }
+        return result;
+    }
+
+
+    /***
+     * 
+     */
+    public String addressFromPubKey( String a ){
+        String result = "addr not found";
+        try {
+            //System.out.println("learn me btc: https://learnmeabitcoin.com/technical/hash-function");
+            //https://gobittest.appspot.com/Address
+            byte[] iti = hexStringToByteArray(a);
+            Sha256Hash itiHash = Sha256Hash.of(iti);
+
+            MessageDigest rmdx = MessageDigest.getInstance("RipeMD160", "BC");
+            byte[] r1x = rmdx.digest(itiHash.getBytes());
+            //Converting the byte array in to HexString format
+            StringBuffer hexString = new StringBuffer();
+            for (int i = 0;i<r1x.length;i++) {
+                hexString.append(Integer.toHexString(0xFF & r1x[i]));
+            }
+
+            byte[] zero = hexStringToByteArray("00");
+            byte[] s5 = new byte[r1x.length + 1];
+            s5[0] = zero[0];
+            System.arraycopy(r1x, 0, s5, 1, r1x.length);
+            
+            Sha256Hash s5x = Sha256Hash.of(s5);
+            Sha256Hash s6x = Sha256Hash.of(s5x.getBytes());
+
+            byte[] first4of6 = new byte[4];
+            System.arraycopy(s6x.getBytes(), 0, first4of6, 0, 4);
+
+            byte[] s8 = new byte[s5.length + first4of6.length];
+
+            System.arraycopy(s5, 0, s8, 0, s5.length);
+            System.arraycopy(first4of6, 0, s8, s5.length, first4of6.length);
+
+            //7 - First four bytes of 6
+            result = Base58.encode(s8);
+        } catch(Exception ex) {
+            System.out.println("address_From_Pub_Key  "+ex.toString());
+            ex.printStackTrace();
+        }
+        return result;
+    }
+
+
+    /* s must be an even-length string. */
+    //https://stackoverflow.com/questions/140131/convert-a-string-representation-of-a-hex-dump-to-a-byte-array-using-java/140861#140861
+    /***
+     * 
+     */
+    public static byte[] hexStringToByteArray(String s) {
+        int len = s.length();
+        byte[] data = new byte[len / 2];
+        for (int i = 0; i < len; i += 2) {
+            data[i / 2] = (byte) ((Character.digit(s.charAt(i), 16) << 4)
+                                + Character.digit(s.charAt(i+1), 16));
+        }
+        return data;
+    }
+    
+
+
+
 }
